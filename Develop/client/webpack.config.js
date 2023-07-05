28cf9e71ad96dd6fc2e42c7aee9c98caeed70d68
Ms Eko <jomaduagwu@gmail.com>
2023-07-05 05:25:34 +0100

    css loader fix

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -53,7 +53,7 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/i,
-          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+          use: ['style-loader', 'css-loader'],
         },
         {
           test: /\.m?js$/,
