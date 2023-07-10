ff8af0e8e7f9c7cd30e684e4d8fb29e408843fc7
Ms Eko <jomaduagwu@gmail.com>
2023-07-10 13:43:49 -0500

    minor edits

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,8 +3,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+
 
 
 module.exports = () => {
@@ -12,7 +11,10 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      database: './src/js/database.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js',
     },
     output: {
       filename: '[name].bundle.js',
@@ -29,7 +31,7 @@ module.exports = () => {
       }),
       
       new WebpackPwaManifest({
-        // TODO: Create a manifest.json:
+        
         fingerprints: false,
         inject: true,
         name: 'Just Another Text Editor',
